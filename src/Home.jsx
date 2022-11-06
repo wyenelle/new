4c5eff2036eddad12a724340de0efcd843f15196
wyenelle <favourkcb@gmail.com>
2022-11-06 23:15:56 +0100

    done

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,7 +5,7 @@ import './Home.css'
 const Home = () => {
   return (
     <div className='home'>
-        <h1> My Name Is  Kiishii</h1>
+        <h1> My Name Is  Smallz</h1>
         <h3> This is my Counter App</h3>
       
     <Link className='linkss' to='/counter'> Use Counter</Link>
